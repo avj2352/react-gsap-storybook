8054fa069ce3525f64643d64192333cf0dc51ede
PRAMOD A JINGADE <avj2352@gmail.com>
2020-05-01 05:48:55 +0800

    initialized project

---
+++ src/app/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './app.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="mt-4 ">React GSAP + Tailwind</h1>
+      <p>The aim of this project is to create reusable components using <span className="cursive highlight">Tailwind, GSAP & Storybook</span></p>
+      <p className="mt-2">The same configuration can also be applied to <span className="cursive highlight">Material UI</span></p>
+    </div>
+  );
+}
+
+export default App;
